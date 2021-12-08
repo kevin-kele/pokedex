5a4940002bbeaf2b650b896c5d8d1f34135af11a
kevin-kele <kevinkele1@gmail.com>
2021-12-08 03:43:06 +0100

    carte pokemon

--- components/Pokemon.vue
+++ components/Pokemon.vue
@@ -1,11 +1,10 @@
 <template>
   <v-card
     v-show="modalInfo"
+    id="padCarte"
     class="mx-auto"
     dark
-    max-width="600"
     elevation="4"
-    card-border-radius="10px"
   >
     <v-card-title>
       <v-icon
@@ -17,12 +16,11 @@
     <div class="border">
       <v-img
         :src="pokemon.url"
-        height="200px"
+        height="300px"
       />
     </div>
-
-    <v-card-text v-for="(power, index) in pokemon.abilities" :key="index" class="text-h6 font-weight-bold">
-      attack : {{ power.ability.name }}
+    <v-card-text v-for="(power, index) in pokemon.powers" :key="index" class="text-h6 pad">
+      <span v-if="power.stat.name === 'attack' || power.stat.name === 'defense' || power.stat.name === 'special-attack' "> {{ power.stat.name }} - {{ power.base_stat }}</span>
     </v-card-text>
 
     <v-card-actions>
@@ -74,5 +72,46 @@ export default {
 }
 .text{
   margin: auto;
+
+}
+.pad{
+  padding:0;
+  margin-top: 10px;
+}
+#padCarte{
+  border-radius: 10px;
+  padding: 5%;
+  border:10px solid yellow;
+}
+.border div {
+position: relative;
+}
+.border div::before {
+position: absolute;
+top: 0;
+left: -75%;
+z-index: 2;
+display: block;
+content: '';
+width: 50%;
+height: 100%;
+background: -webkit-linear-gradient(left, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+background: linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,.3) 100%);
+-webkit-transform: skewX(-25deg);
+transform: skewX(-25deg);
+}
+.border div:hover::before {
+-webkit-animation: shine .75s;
+animation: shine .75s;
+}
+@-webkit-keyframes shine {
+100% {
+left: 125%;
+}
+}
+@keyframes shine {
+100% {
+left: 125%;
+}
 }
 </style>>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,47 @@
 <template>
-  <div class="content">
-    <div class="m">
-      <div v-for="(pokemon, index) in pokemons" :key="index" class="mr">
-        <v-card
-          class="mx-auto m-2"
-          max-width="344"
-          width="200px"
-        >
-          <v-img
-            :src="pokemon.url"
-            height="200px"
-          />
+  <div>
+    <v-img
+      class="center"
+      width="700px"
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/2560px-International_Pok%C3%A9mon_logo.svg.png"
+    />
+    <div class="content">
+      <div class="m">
+        <div v-for="(pokemon, index) in pokemons" :key="index" class="mr">
+          <v-card
+            class="mx-auto m-2"
+            width="300px"
+          >
+            <v-img
+              :src="pokemon.url"
+              height="300px"
+            />
 
-          <v-card-title>
-            {{ pokemon.name }}
-          </v-card-title>
-          <v-card-actions>
-            <v-btn
-              color="primary"
-              dark
-              @click="infoPokemon(pokemon)"
-            >
-              Explore
-            </v-btn>
-            <v-spacer />
-          </v-card-actions>
-        </v-card>
+            <v-card-title>
+              {{ pokemon.name }}
+            </v-card-title>
+            <v-card-actions>
+              <v-btn
+                color="primary"
+                dark
+                @click="infoPokemon(pokemon)"
+              >
+                Explore
+              </v-btn>
+              <v-btn
+                color="red"
+                dark
+              >
+                Add equipe
+              </v-btn>
+              <v-spacer />
+            </v-card-actions>
+          </v-card>
+        </div>
       </div>
-    </div>
-    <div class="fix">
-      <Pokemon :pokemon="pokeData" :modal-info="modalInfo" @close-modal="closeModal" />
+      <div class="fix">
+        <Pokemon :pokemon="pokeData" :modal-info="modalInfo" @close-modal="closeModal" />
+      </div>
     </div>
   </div>
 </template>
@@ -56,6 +68,7 @@ export default {
       const pokemon = {
         name: reponse.data.name,
         abilities: reponse.data.abilities,
+        powers: reponse.data.stats,
         url: reponse.data.sprites.front_default
       }
       instance.pokemons.push(pokemon)
@@ -80,20 +93,26 @@ export default {
 .m{
   display: flex;
   flex-wrap: wrap;
-  width: 50%;
-  margin-right: 30%;
+  margin-right: auto;
+  margin-left: 10%;
 }
 .content{
   display: flex;
   justify-content: space-around;
 }
 .mr{
-  margin-right: 60px;
+  margin-right: 10px;
   margin-bottom: 10px;
 }
 .fix{
+  width: 400px;
+  margin-right: 5%;
+  margin-top: -5%;
   position: fixed;
-  margin-left: 55%;
-  width: 350px;
+}
+.center{
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2%;
 }
 </style>
